326c3ff94aaf3c8ce995e9a79b70f06ceb09d3ca
unknown <aniketsjadhav1998@gmail.com>
2024-08-13 00:50:48 +0530

    Added Kaju katli and Pepsi

--- hungry.py
+++ hungry.py
@@ -3,11 +3,13 @@ if hu=="yes":
     print("Eat Samosa")
     print("Eat Fries")
     print('Eat Burger')
+    print('Eat Kaju katli')
 else:
     thirsty=input('Are you thirsty?')
     if thirsty=='yes':
         print('Drink water')
         print('Drink Soda')
+        print('Drink Pepsi')
     else:
         print('Do homework')
 
