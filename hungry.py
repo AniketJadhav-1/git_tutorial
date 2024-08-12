8f8c86c3329a1d578d519696e9b1c7d0f286c341
unknown <aniketsjadhav1998@gmail.com>
2024-08-13 00:23:03 +0530

    Added Vadapav line

--- hungry.py
+++ hungry.py
@@ -3,6 +3,7 @@ if hu=="yes":
     print("Eat Samosa")
     print("Eat Fries")
     print('Eat Burger')
+    print('Eat Vadapav')
 else:
     thirsty=input('Are you thirsty?')
     if thirsty=='yes':
